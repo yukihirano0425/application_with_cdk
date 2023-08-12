9adae4df58e9486047cb2916c56f4a17a225cf84
yukihirano0425 <yuki.hirano@uzabase.com>
2023-08-12 16:17:41 +0900

    メール通知の実装

--- sample/lib/cdk-app-stack.ts
+++ sample/lib/cdk-app-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as subscriptions from "aws-cdk-lib/aws-sns-subscriptions";
 import { Construct } from "constructs";
 
 export class CdkAppStack extends cdk.Stack {
@@ -13,5 +15,12 @@ export class CdkAppStack extends cdk.Stack {
     const outputBucket = new s3.Bucket(this, "cdk-app-output-bucket", {
       bucketName: "cdk-app-output-bucket",
     });
+
+    const emailTopic = new sns.Topic(this, "Topic", {
+      topicName: "cdk-app-topic",
+    });
+
+    const email = "yu1434soccer@gmail.com";
+    emailTopic.addSubscription(new subscriptions.EmailSubscription(email));
   }
 }
